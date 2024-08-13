5fa3fc7b9b7b1e5cf1ff78ae13c578eabefcc382
giordanofabrizii <giordanofabrizi@gmail.com>
2024-08-13 18:32:38 +0200

    fixed game finish and add event on enter key

--- game-logic.js
+++ game-logic.js
@@ -40,7 +40,14 @@ colorKeys.forEach(colorKey => {
 
 // submit event
 let submitEl = document.getElementById("submit");
-submitEl.addEventListener("click", () => {
+submitEl.addEventListener("click", enter)
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') { 
+        enter();
+    }
+});
+
+function enter() {
     // start the function
     colorsInput = [];
     inputEls.forEach(inputEl => {
@@ -68,6 +75,7 @@ submitEl.addEventListener("click", () => {
         switch (result) {
             case false:
                 correctIndexEl.innerHTML = 0;
+                resetInput();
                 break;
             case true:
                 resultsEl.innerHTML = "YOU WON";
@@ -76,9 +84,10 @@ submitEl.addEventListener("click", () => {
                 break;
             default:
                 correctIndexEl.innerHTML = result.length;
+                resetInput();
             }
         tryIndex++;
-        if (tryIndex >= 5) {
+        if (tryIndex >= 6) {
             resultsEl.innerHTML = "YOU LOSE"
             document.getElementById("game").style.display = "none";
             document.getElementById("results").style.display = "block";
@@ -87,7 +96,7 @@ submitEl.addEventListener("click", () => {
     } else {
         document.getElementById("color-length").style.display = "block"
     }
-});
+};
 
 /**
  * Generate a list of 5 colors chosen between the available all differents from eachother
@@ -130,11 +139,9 @@ function controlWin(tryList, correctList){
 }
 
 function restart(){
-    selectedIndex = 0;
     tryIndex = 1;
     combination = randomCombination();
     colorsInput = [];
-    let controlList = document.querySelectorAll(".row.control > *");
 
     for (let i = 0; i < 5; i ++){
         rows[i].innerHTML = [];
@@ -143,12 +150,22 @@ function restart(){
             el.classList = "color";
             rows[i].appendChild(el);
         }
+    }
+
+    resetInput();
+
+    document.getElementById("errors").style.display = "none";
+    document.getElementById("results").style.display = "none";
+    document.getElementById("game").style.display = "block";
+}
+
+function resetInput() {
+    let controlList = document.querySelectorAll(".row.control > *");
+    selectedIndex = 0;
+    for (let i = 0; i < 5; i ++){
         controlList[i].classList = "color";
         if (i == 0) {
             controlList[i].classList.add("selected");
-        }
+        }       
     }
-
-    document.getElementById("results").style.display = "none";
-    document.getElementById("game").style.display = "block";
 }
